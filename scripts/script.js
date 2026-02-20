3577328e53b641c734605613255da0cba258c778
Rodrigo Andrade <soadse.ra@gmail.com>
2026-02-19 21:26:39 -0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,42 @@
 let grid = document.querySelector('.grid-produtos')
+let grid_moda_verao = document.querySelector('.grid-produtos-verao')
+let grid_moda_inverno = document.querySelector('.grid-produtos-inverno')
 
-fetch("data.json")
-.then(response => response.json())
-.then(dados => {
-    dados.forEach((e) => {
-        let img = document.createElement('img')
-        img.src = e.imagem
+let dados_produtos = "produtos.json"
+let dados_moda_verao = "moda_verao.json"
+let dados_moda_inverno = "moda_inverno.json"
 
-        let categoria = document.createElement('p')
-        categoria.innerText = e.categoria
+function buscaDados(base, grid) {
+    fetch(base)
+    .then(response => response.json())
+    .then(dados => {
+        dados.forEach((e) => {
+            let img = document.createElement('img')
+            img.src = e.imagem
 
-        let produto = document.createElement('h3')
-        produto.innerText = e.produto
+            let categoria = document.createElement('p')
+            categoria.innerText = e.categoria
 
-        let preco = document.createElement('p')
-        preco.innerText = e. preco
+            let produto = document.createElement('h3')
+            produto.innerText = e.produto
 
-        let btn = document.createElement('btn')
-        btn.innerText = "Saiba Mais"
+            let preco = document.createElement('p')
+            preco.innerText = e. preco
 
-        let div = document.createElement('div')
-        div.appendChild(img)
-        div.appendChild(categoria)
-        div.appendChild(produto)
-        div.appendChild(preco).classList.add('price')
-        div.appendChild(btn).classList.add('btn')
+            let btn = document.createElement('btn')
+            btn.innerText = "Saiba Mais"
 
-        grid.appendChild(div).classList.add('card')
-    });
-})
+            let div = document.createElement('div')
+            div.appendChild(img)
+            div.appendChild(categoria)
+            div.appendChild(produto)
+            div.appendChild(preco).classList.add('price')
+            div.appendChild(btn).classList.add('btn')
+
+            grid.appendChild(div).classList.add('card')
+        });
+    })
+}
+buscaDados(dados_produtos, grid)
+buscaDados(dados_moda_verao, grid_moda_verao)
+buscaDados(dados_moda_inverno, grid_moda_inverno)
